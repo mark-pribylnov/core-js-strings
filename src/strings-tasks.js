ccc6ca34f671803c9e56dbf68d9daef31a2f2312
unknown <mark.pribylnov@gmail.com>
2025-09-15 12:32:23 +0300

    feat: solve a few more tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -333,8 +333,30 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  function createAlphabet() {
+    const alphabetLower = [];
+    for (let i = 97; i <= 122; i += 1) {
+      alphabetLower.push(String.fromCharCode(i));
+    }
+
+    const alphabetUpper = [];
+    for (let i = 65; i <= 90; i += 1) {
+      alphabetUpper.push(String.fromCharCode(i));
+    }
+
+    return [...alphabetUpper, ...alphabetLower];
+  }
+  const alphabet = createAlphabet();
+
+  const chars = str
+    .split('')
+    .filter((letter) => alphabet.includes(letter))
+    .map((letter) => letter.toLowerCase());
+
+  const charsReversed = chars.slice().reverse();
+
+  return charsReversed.join('') === chars.join('');
 }
 
 /**
--- tst.js
+++ tst.js
@@ -1,12 +1,74 @@
 function getFirstChar(str) {
-  let sum = 0;
-  console.log(str.split(''));
+  function createAlphabet() {
+    const alphabetLower = [];
+    for (let i = 97; i <= 122; i += 1) {
+      alphabetLower.push(String.fromCharCode(i));
+    }
 
-  str.split('').forEach((item) => {
-    sum += item.charCodeAt(0);
-  });
+    const alphabetUpper = [];
+    for (let i = 65; i <= 90; i += 1) {
+      alphabetUpper.push(String.fromCharCode(i));
+    }
 
-  return sum;
+    return [...alphabetUpper, ...alphabetLower];
+  }
+  const alphabet = createAlphabet();
+
+  const chars = str
+    .split('')
+    .filter((letter) => alphabet.includes(letter))
+    .map((letter) => letter.toLowerCase());
+
+  const charsReversed = chars.slice().reverse();
+
+  return charsReversed.join('') === chars.join('');
+
+  // console.log(charsReversed.join(''));
+  // console.log(chars.join(''));
+
+  // const lowerCased = str
+  //   .split('')
+  //   .map((letter) => letter.toLowerCase())
+  //   .join('');
+
+  // const reversedLowerCased = lowerCased.split('').reverse().join('');
+
+  // // console.log(lowerCased);
+  // console.log(reversed);
+  // // console.log(reversedLowerCased);
 }
 
-console.log(getFirstChar());
+console.log(getFirstChar('A man, a plan, a canal, Panama!'));
+
+// function getFirstChar(str) {
+//   const lowerCased = str
+//     .split('')
+//     .map((letter) => letter.toLowerCase())
+//     .join('');
+
+//   let reversed = str.split('').reverse().join('');
+//   const reversedLowerCased = lowerCased.split('').reverse().join('');
+
+//   // console.log(lowerCased);
+//   console.log(reversed);
+//   // console.log(reversedLowerCased);
+
+//   if ('?!'.includes(reversed[0])) {
+//     const splittedReversed = reversed.split('');
+//     const shiftedElem = splittedReversed.shift();
+
+//     splittedReversed.push(shiftedElem);
+
+//     splittedReversed[0] = splittedReversed[0].toUpperCase();
+//     splittedReversed[splittedReversed.length - 2] =
+//       splittedReversed[splittedReversed.length - 2].toLowerCase();
+
+//     reversed = splittedReversed.join('');
+//   }
+//   console.log(reversed);
+
+//   if (reversed === lowerCased) {
+//     return true;
+//   }
+//   return false;
+// }
